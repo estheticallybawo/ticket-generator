16b39889055acce2b5b82da91b6d9951e3b036be
Esther <estheticallybawo@gmail.com>
2025-02-15 21:36:25 +0100

    Font to Global

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,13 @@
 import { TicketProvider } from "./context/TicketContext";
 import Home from "./Home/main-page";
 import Header from "./components/Header";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <div>
+      <Link rel="stylesheet"
+        href={"https://fonts.googleapis.com/css2?family=Alatsi&family=Road+Rage&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"}></Link>
       <Header />
       <TicketProvider>
         <Home />
